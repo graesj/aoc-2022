b0ff314422a09586a424a4c9a9e49ceaa56341dc
graesj <hakongrotte@gmail.com>
2022-12-01 21:53:20 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.toIntOrNull() }
+            .runningFold(0) { acc, calories ->
+                calories?.let {
+                    acc + it
+                } ?: 0
+            }.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map { it.toIntOrNull() }
+            .runningFold(0) { acc, calories ->
+                calories?.let {
+                    acc + it
+                } ?: 0
+            }.sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
